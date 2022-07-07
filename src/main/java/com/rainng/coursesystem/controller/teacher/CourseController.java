a48128fc7b43aa4fe62d10cb10add417f180e58e
ChrisDing090 <1289224573@qq.com>
2022-07-07 10:50:32 +0800

    hotfix commit 1

--- src/main/java/com/rainng/coursesystem/controller/teacher/CourseController.java
+++ src/main/java/com/rainng/coursesystem/controller/teacher/CourseController.java
@@ -20,6 +20,7 @@ public class CourseController extends BaseController {
     @RequestMapping("/list")
     public ResultVO list() {
         System.out.println(service.list());
+        System.out.println(1);
         return service.list();
     }
 }
